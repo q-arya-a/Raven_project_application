b6d2950f055e552812e74e1384180a15c0a69d5f
Arya <152710342+q-arya-a@users.noreply.github.com>
2024-12-22 22:05:07 +0530

    Update Raven_ptr_qn.cpp

--- Raven_ptr_qn.cpp
+++ Raven_ptr_qn.cpp
@@ -5,14 +5,18 @@ class my_unique_ptr{
         T* ptr = nullptr;
 
     public:
-        //constructor with no arguments
-        my_unique_ptr() : ptr(nullptr) {}
+        //default constructor
+        my_unique_ptr() : ptr(nullptr) {} // Initialize with nullptr
 
         //constructor
-        my_unique_ptr(T* ptr) : ptr(ptr) {}
+        my_unique_ptr(T* ptr) { this->ptr = ptr; }
+
+        //destructor
+        ~my_unique_ptr(){
+            delete ptr;
+        }
 
         /*Remove copy semantics*/
-
         my_unique_ptr(const my_unique_ptr & obj) = delete;
         //copy constructor is deleted
 
@@ -20,16 +24,16 @@ class my_unique_ptr{
         //copy assignment is deleted
 
         //move constructor
-        my_unique_ptr(my_unique_ptr&& other){
+        my_unique_ptr(my_unique_ptr&& other) noexcept {
             //Transfer ownership of memory pointed by other
             ptr = other.ptr;
             other.ptr = nullptr;
         }
 
         //move assignment operator
-        my_unique_ptr& operator=(my_unique_ptr&& other){
+        my_unique_ptr& operator=(my_unique_ptr&& other) noexcept {
             //prevent self assignment
-            if(this != other){
+            if(this != &other){
                 delete ptr;     //free already existing resource
                 ptr = other.ptr;      //Transfer the ownership
                 other.ptr = nullptr;
@@ -42,7 +46,7 @@ class my_unique_ptr{
         T* operator->() const {return ptr; }
 
         // check if the pointer is not null
-        explicit operator bool() const (return ptr != nullptr; )
+        explicit operator bool() const {return ptr != nullptr; }
 
         //release ownership of the managed object
         T* release() {
@@ -53,8 +57,23 @@ class my_unique_ptr{
 
         //replace the managed object
         void reset(T* other = nullptr){
+            if (ptr != other){
             delete ptr;
             ptr = other;
+            }
         }
 
 };
+#include <iostream>
+// Test class
+class Test {
+public:
+    void show() {
+        std::cout << "Test::show() called!" <<std::endl;
+    }
+
+    ~Test() {
+        std::cout << "Test object destroyed!" << std::endl;
+    }
+};
+
